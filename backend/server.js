cb8892b3209a748e8a629ee15e16bc7ba2b823c2
ebincodes5599 <ebincodes5599@gmail.com>
2024-06-09 16:09:24 +0100

    changing some errors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,87 @@
 
 
-import express from 'express';
+// import express from 'express';
+// // import cors from 'cors';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// // import rootRoutes from './routes/root.routes.js';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import cookieParser from 'cookie-parser';
+
+// // Construct __dirname for ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Load environment variables, assuming .env is in the root directory
+// dotenv.config({ path: path.join(__dirname, '../.env') });
+// const app = express();
+// import authRoutes from "./routes/auth.routes.js";
+// import messageRoutes from "./routes/message.routes.js";
+// import userRoutes from "./routes/user.routes.js";
 // import cors from 'cors';
+
+
+
+// // import connectToMongoDB from "./db/connectToMongoDB.js";
+// // import { app, server } from "./socket/socket.js";
+
+
+// const PORT = process.env.PORT || 5000;
+
+
+
+// mongoose
+//   .connect(process.env.MONGO, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     writeConcern: {
+//       w: 'majority',
+//     },
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+//   })
+//   .catch((err) => {
+//     console.log('Error connecting to MongoDB:', err);
+//   });
+
+
+
+// // app.use(cors());
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//   origin: 'http://localhost:00',
+//   credentials: true,
+// }));
+// app.use((req, res, next) => {
+//   console.log('Received request:', req.method, req.path);
+//   next();
+// });
+
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
+// // app.use(express.static(path.join(__dirname, "/frontend/chat-app/dist")));
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "../frontend",  "dist", "index.html"));
+// });
+
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import rootRoutes from './routes/root.routes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
+import http from 'http';
 
 // Construct __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -21,15 +95,7 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cors from 'cors';
 
-
-
-// import connectToMongoDB from "./db/connectToMongoDB.js";
-// import { app, server } from "./socket/socket.js";
-
-
-const PORT = process.env.PORT || 5000;
-
-
+const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO, {
@@ -46,9 +112,6 @@ mongoose
     console.log('Error connecting to MongoDB:', err);
   });
 
-
-
-// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -60,18 +123,15 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// app.use(express.static(path.join(__dirname, "/frontend/chat-app/dist")));
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontend",  "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
+const server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
